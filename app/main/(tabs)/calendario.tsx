199b4fb11b2a537078052c3e8bf1f0c3eaa6002b
leowbk77 <leonardoptc10@gmail.com>
2025-03-21 14:13:04 -0300

    alfa do calendario

--- app/main/(tabs)/calendario.tsx
+++ app/main/(tabs)/calendario.tsx
@@ -1,17 +1,60 @@
 import { Text, View, ImageBackground, StyleSheet } from 'react-native';
- import { Link } from 'expo-router'; 
+import { CalendarBody, CalendarContainer, CalendarHeader, LocaleConfigsProps } from '@howljs/calendar-kit';
+import { Link } from 'expo-router';
+import { Dimensions } from 'react-native';
+
+/*
+=======calendario
+https://howljs.github.io/react-native-calendar-kit/docs/intro
+meio quebrado^ mas funciona
+====================================
+*/
 
 export default function Calendario() {
+  
+  const initialLocales: Record<string, Partial<LocaleConfigsProps>> = {
+    en: {
+      weekDayShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'), // Text in day header (Sun, Mon, etc.)
+      meridiem: { ante: 'am', post: 'pm' }, // Hour format (hh:mm a)
+      more: 'more', // Text for "more" button (All day events)
+    },
+    ja: {
+      weekDayShort: '日_月_火_水_木_金_土'.split('_'),
+      meridiem: { ante: '午前', post: '午後' },
+      more: 'もっと',
+    },
+    vi: {
+      weekDayShort: 'CN_T2_T3_T4_T5_T6_T7'.split('_'),
+      meridiem: { ante: 'sa', post: 'ch' },
+      more: 'Xem thêm',
+    },
+    ptBR: {
+      weekDayShort: 'Dom_Seg_Ter_Qua_Qui_Sex_Sab'.split('_'),
+      meridiem: { ante: 'antes', post: 'depois' },
+      more: 'mais',
+    },
+  };
+
   return (
     <ImageBackground
     source={require('@/assets/images/focavestbkg.jpg')}
     style={styles.backgroundImg}
     resizeMode='cover'>
       <View>
-        <Text style={styles.text}>Calendário</Text>
+        <View style={styles.calendarView}>
+          <CalendarContainer
+            calendarWidth={Dimensions.get('window').width}
+            initialLocales={initialLocales}
+            locale='ptBR'>
+            <CalendarHeader />
+            <CalendarBody />
+          </CalendarContainer>
+        </View>
+
         <Link href="/" style={styles.button}>
           Voltar
         </Link>
+
       </View>
     </ImageBackground>
   );
@@ -23,12 +66,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  text: {
-    color: 'black',
-  },
   button: {
     fontSize: 20,
     textDecorationLine: 'underline',
     color: 'black',
+    marginTop: '5%',
+    alignSelf: 'center',
   },
+  calendarView: {
+    height: '60%',
+  }
 });
